d9a778cbb0d8da22cba34191fbe05c91a117a613
ErikBoesen <me@erikboesen.com>
2019-12-30 19:14:20 -0500

    Add iOS download link

--- js/download.js
+++ js/download.js
@@ -22,7 +22,7 @@ let os = getOS();
 if (os === "Android") {
     window.location.replace("https://play.google.com/store/apps/details?id=com.erikboesen.comethru");
 } else if (os === "iOS") {
-    window.location.replace("/");
+    window.location.replace("https://apps.apple.com/us/app/comethru-yale/id1487264399");
 } else {
     window.location.replace("/");
 }
